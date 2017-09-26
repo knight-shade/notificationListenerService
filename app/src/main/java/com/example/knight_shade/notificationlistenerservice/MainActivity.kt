2ed27c4fe0f5fc892c6a1a303e4ed905c0f8b42e
Abhishek Poonia <abhishekpoonia22@gmail.com>
2017-09-26 20:08:50 +0530

    initial commit

---
+++ app/src/main/java/com/example/knight_shade/notificationlistenerservice/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.knight_shade.notificationlistenerservice
+
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.app.NotificationCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
